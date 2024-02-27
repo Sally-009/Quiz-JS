279a2493e86808b20177eed7b35057566286b4a9
Saori Kojima <skojima@atu.edu>
2024-02-27 17:41:57 -0600

    added quiz dataset

---
+++ src/quiz_dataset.js
@@ -0,0 +1,16 @@
+// the question types
+const QUESTION_TYPES = {
+  TRUE_FALSE: 'true_false',
+  MULTIPLE_CHOICE: 'multiple_choice',
+};
+
+// question sets
+const questionSets = [
+  {
+    id: 1,
+    type: QUESTION_TYPES.TRUE_FALSE,
+    text: 'Sally is from Japan.',
+    correctAnswer: true,
+  },
+  {},
+];
