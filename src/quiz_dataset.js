d67f240a41438f625b05a4f264cdc59400e19806
Saori Kojima <skojima@atu.edu>
2024-03-01 22:18:44 -0600

    added Question set

--- src/quiz_dataset.js
+++ src/quiz_dataset.js
@@ -8,9 +8,26 @@ const QUESTION_TYPES = {
 const questionSets = [
   {
     id: 1,
-    type: QUESTION_TYPES.TRUE_FALSE,
-    text: 'Sally is from Japan.',
-    correctAnswer: true,
+    type: QUESTION_TYPES.MULTIPLE_CHOICE,
+    text: 'What is the breed of this dog?',
+    image: 'images/Yorkie.jpg',
+    answers: ['Pomeranian', 'Chihuahua', 'Yorkshire Terrier', 'Shih-Tzu'],
+    correctAnswer: 'Yorkshire Terrier',
   },
-  {},
+  {
+    id: 2,
+    type: QUESTION_TYPES.MULTIPLE_CHOICE,
+    text: 'What is the breed of this dog?',
+    image: 'images/GoldenRetriever.jpg',
+    answers: ['Golden Retriever', 'German Shepherd', 'Labrador Retriever', 'Rottweiler'],
+    correctAnswer: 'Golden Retriever',
+  },
+  {
+    id: 3,
+    type: QUESTION_TYPES.MULTIPLE_CHOICE,
+    text: 'What is the breed of this dog?',
+    image: 'images/Shiba.jpg',
+    answers: ['Border Collie', 'Shiba-Inu', 'Siberian Husky', 'Bulldog'],
+    correctAnswer: 'ShiBa-Inu',
+  },
 ];
